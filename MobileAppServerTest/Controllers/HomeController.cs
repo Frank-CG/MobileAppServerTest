f54ca23deeb0e3e8ff16d1b7d327e5384ba87e6a
frankg <frankg@sliq.com>
2019-07-26 15:26:44 -0400

    Mobile app server for testing FCM notification.

---
+++ MobileAppServerTest/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MobileAppServerTest.Models;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using FirebaseAdmin.Messaging;
+
+namespace MobileAppServerTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> SendMessageAsync()
+        {
+            
+            // The topic name can be optionally prefixed with "/topics/".
+            var topic = "topic";
+
+            // See documentation on defining a message payload.
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    { "id", "850" },
+                    { "status", "2:45" },
+                },
+                Topic = topic,
+                Notification = new Notification()
+                {
+                    Body = "Frank send this message from server side.",
+                    Title ="Sliq FCM Notification Testing"
+                },
+            };
+
+            // Send a message to the devices subscribed to the provided topic.
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+            // Response is a message ID string.
+            Console.WriteLine("Successfully sent message: " + response);
+            ViewData["Message"] = "Successfully sent message: " + response;
+            return View();
+        }
+    }
+}
